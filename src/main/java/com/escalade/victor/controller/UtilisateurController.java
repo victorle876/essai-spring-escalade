89e3d794d661d201041bd4638683048664f6a893
victorle876 <victorl648@gmail.com>
2019-12-18 22:24:36 +0100

    remise à plat - update

--- src/main/java/com/escalade/victor/controller/UtilisateurController.java
+++ src/main/java/com/escalade/victor/controller/UtilisateurController.java
@@ -1,6 +1,5 @@
 package com.escalade.victor.controller;
 
-
 import com.escalade.victor.model.Utilisateur;
 
 import com.escalade.victor.repository.UtilisateurRepository;
@@ -15,103 +14,55 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 @Controller
 public class UtilisateurController {
 
     @Autowired
     UtilisateurRepository utilisateurRepository;
-    private List <Utilisateur> utilisateurs = new ArrayList<Utilisateur>();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home (Model model) {
+    public String home(Model model) {
         model.addAttribute("utilisateurs", this.utilisateurRepository.findAll());
-        return "home.html";
+        return "home";
     }
 
 
-   @RequestMapping(value = "/add", method = RequestMethod.GET)
-   public String ajouter (Model model) {
-       model.addAttribute("utilisateur", new Utilisateur());
-        return "add.html";
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String ajouter(Model model) {
+        model.addAttribute("utilisateur", new Utilisateur());
+        return "add";
     }
-    //
 
-    //  utilisateur remplace par note
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String sauver (@Valid @ModelAttribute Utilisateur utilisateur2, Model model) {
-
-        utilisateurs.add(utilisateur2);
-        System.out.println(utilisateur2);
-//        model.addAttribute("utilisateurs", this.utilisateurs);
-        model.addAttribute("utilisateurs", this.utilisateurRepository.save(utilisateur2));
-        return "home.html";
+    public String save(@Valid @ModelAttribute Utilisateur utilisateur, Model model) {
+        this.utilisateurRepository.save(utilisateur);
+        model.addAttribute("utilisateurs", this.utilisateurRepository.findAll());
+        return "redirect:home";
     }
 
-    /*@RequestMapping(value = "/details", method = RequestMethod.GET)
-    public String detail(@RequestParam(value = "id") Integer id, Model model) {*/
-        @RequestMapping(value = "/details", method = RequestMethod.GET)
-        public String detail(@RequestParam(value = "id") Long id, Model model) {
-
-/*        for (Utilisateur utilisateur : this.utilisateurs) {
-            if (utilisateur.getId().equals(id)) {
-
-               model.addAttribute("utilisateur", utilisateur);
-               return "details.html";
-
-            }
-        }
-          return null;*/
-        System.out.println(id);
-
-        Utilisateur utilisateur3 = this.utilisateurRepository.findById(id).get();
-        model.addAttribute("utilisateur", utilisateur3);
-        return "details.html";
+    @RequestMapping(value = "/details", method = RequestMethod.GET)
+    public String detail(@RequestParam(value = "id") Long id, Model model) {
+        model.addAttribute("utilisateur", this.utilisateurRepository.findById(id).get());
+        return "details";
 
     }
 
-   @RequestMapping(value = "/edition", method = RequestMethod.GET)
-//    public String edition(@RequestParam(value = "id") Integer id, Model model) {
-      public String edition(@RequestParam(value = "id") Long id, Model model) {
-
-/*        System.out.println(id);
-        for (Utilisateur utilisateur : this.utilisateurs) {
-            if (utilisateur.getId().equals(id)) {
-                // utilisateurs.set(id-1, utilisateur);
-                model.addAttribute("utilisateur", utilisateur);
-                return "edition.html";
-
-            }
-        }
-        return null;*/
-       Utilisateur utilisateur3 = this.utilisateurRepository.findById(id).get();
-       model.addAttribute("utilisateur", utilisateur3);
-       return "edition.html";
+    @RequestMapping(value = "/edition", method = RequestMethod.GET)
+    public String edition(@RequestParam(value = "id") Long id, Model model) {
+        model.addAttribute("utilisateur", this.utilisateurRepository.findById(id).get());
+        return "edition";
 
     }
 
-    @RequestMapping(value = "/edition2", method = RequestMethod.POST)
-//    public String edition2 (@RequestParam(value = "id") Integer id, @Valid @ModelAttribute Utilisateur utilisateur, BindingResult errors, Model model) {
-      public String edition2 (@RequestParam(value = "id") long id, @Valid @ModelAttribute Utilisateur utilisateur, BindingResult errors, Model model) {
-/*        System.out.println(utilisateur);
-        for (Utilisateur u : this.utilisateurs) {
-            if (u.getId().equals(id)) {
-                this.utilisateurs.set(id-1, utilisateur);
-
-                model.addAttribute("utilisateurs", this.utilisateurs);
-                return "home.html";
-
-            }
-        }return null; 4*/
-
-         Utilisateur utilisateur3 = this.utilisateurRepository.findById(id).get();
-         utilisateur3.setId(id);
-         Utilisateur updateUtilisateur = this.utilisateurRepository.save(utilisateur3);
-         model.addAttribute("utilisateur", updateUtilisateur);
-       return "home.html";
-        }
-
-
+    @RequestMapping(value = "/edition", method = RequestMethod.POST)
+    public String edition2(@RequestParam(value = "id") long id, @Valid @ModelAttribute Utilisateur utilisateur, BindingResult errors, Model model) {
+        this.utilisateurRepository.save(utilisateur);
+        model.addAttribute("utilisateurs", this.utilisateurRepository.findAll());
+        return "redirect:/";
     }
+
+
+}
